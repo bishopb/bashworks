a80513a1bad2741901c14a689742c7f4cf56322f
Bishop Bettini <bishop.bettini@gmail.com>
2015-09-15 10:16:28 -0400

    Adds some interactive helpfulnesses.

--- interactive/functions.sh
+++ interactive/functions.sh
@@ -15,3 +15,24 @@ function interactive_start_agent {
     . "${SSH_ENV}" > /dev/null
     /usr/bin/ssh-add $(find "$HOME"/.ssh/ -perm 0600 -name id\*)
 }
+
+# interactive_diceware -- Throw some dice and get some words
+function interactive_diceware {
+    numWords=${1:-5}
+    srclist="http://world.std.com/~reinhold/beale.wordlist.asc"
+    wordlist="${TMPDIR:-/tmp}/diceware.wordlist.asc"
+
+
+    if [ ! -e "$wordlist" ]; then
+        curl -sS "$srclist" > "$wordlist"
+    fi
+
+    for w in $(seq 1 $numWords); do
+       digits=
+       for d in 1 2 3 4 5; do
+         digit=$(shuf --random-source=/dev/random -i 1-6 -n 1)
+         digits="$digits$digit"
+       done
+       grep "^$digits\s" "$wordlist"
+    done
+}
