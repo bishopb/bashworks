90fa6507bf8e74544ab61fa4022339025243dd09
Bishop Bettini <bishop.bettini@gmail.com>
2016-04-07 15:22:20 -0400

    I am tired of typing fg

--- interactive/aliases.sh
+++ interactive/aliases.sh
@@ -1,6 +1,27 @@
+# jobs related
 # fj -- Find jobs matching a pattern
 alias fj='jobs | egrep -ie'
-
+alias  1='fg 1'
+alias  2='fg 2'
+alias  3='fg 3'
+alias  4='fg 4'
+alias  5='fg 5'
+alias  6='fg 6'
+alias  7='fg 7'
+alias  8='fg 8'
+alias  9='fg 9'
+alias 10='fg 10'
+alias 11='fg 11'
+alias 12='fg 12'
+alias 13='fg 13'
+alias 14='fg 14'
+alias 15='fg 15'
+alias 16='fg 16'
+alias 17='fg 17'
+alias 18='fg 18'
+alias 19='fg 19'
+alias -- -='fg -'
+alias -- +='fg +'
 # composer -- Short cut running local composer
 alias composer='php composer.phar'
 
