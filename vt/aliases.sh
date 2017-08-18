e6f703f70375fed6c4afe3a1b2f51541f742ca2a
Bishop Bettini <bishop.bettini@gmail.com>
2017-08-18 12:14:29 -0400

    Fixes incorrect type

--- vt/aliases.sh
+++ vt/aliases.sh
@@ -1,5 +1,5 @@
 # finding stuff
-alias ff='rg -t js -t json -t php -t md -t yaml -t xml -t twig -t README -g !coverage -g !include/vendor -g !node_modules -i'
+alias ff='rg -t js -t json -t php -t md -t yaml -t xml -t twig -t readme -g !coverage -g !include/vendor -g !node_modules -i'
 
 # jobs related
 alias tj='vt_test_job_in_edit'
