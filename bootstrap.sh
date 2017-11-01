8f9c87b281e17e7ff2a46f4ffccd6993a27c5dd1
Bishop Bettini <bishop.bettini@gmail.com>
2017-11-01 10:48:58 -0400

    Be a little bit more explicit

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,12 @@
 # Load the framework
-source ~/bashworks/module/source.sh
-module_repo ~/bashworks
+source "${HOME}"/bashworks/module/source.sh
+module_repo "${HOME}"/bashworks
 
 # Load common tools
 module toolbox
 
 # When interactive, load the stuff I use
 [[ $- = *i* ]] && { 
-    module interactive vt
+    module interactive
+    module vt
 }
