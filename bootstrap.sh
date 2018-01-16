c4e127391a75d0f4277d8e707330a5f7eabdd843
Bishop Bettini <bishop.bettini@gmail.com>
2018-01-16 10:53:42 -0500

    Mandate a screen

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,9 @@ module_repo "${HOME}"/bashworks
 # Load common tools
 module toolbox
 
-# When interactive, load the stuff I use
+# When interactive, load the stuff I use and fire up screen
 [[ $- = *i* ]] && { 
     module interactive
     module vt
+    interactive_changescreen
 }
